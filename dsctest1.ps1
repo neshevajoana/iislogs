13950eab25a7528ef07439ededed1815266c2bbe
neshevajoana <53709092+neshevajoana@users.noreply.github.com>
2020-04-25 16:00:27 -0400

    Add files via upload

---
+++ dsctest1.ps1
@@ -0,0 +1,27 @@
+﻿configuration WebServer
+{
+    Node MyVM
+    {
+        WindowsFeature IIS
+        {
+            Ensure               = 'Present'
+            Name                 = 'Web-Server'
+            IncludeAllSubFeature = $true
+        }
+
+        File HelloWorldlog
+        {
+            Ensure = 'Present'
+            DestinationPath = "C:\inetpub\logs\LogFiles\LogFile.log"
+            Contents = "Hello World,Yoana!"
+        }
+         File HelloWorldlog1
+        {
+            Ensure = 'Present'
+            DestinationPath = "C:\inetpub\logs\LogFile.log"
+            Contents = "Hello World,Yoana!"
+        }
+           
+    }
+}
+WebServer
