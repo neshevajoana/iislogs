96d484423e58434b0d2908274b51b58d8966c486
neshevajoana <53709092+neshevajoana@users.noreply.github.com>
2020-04-25 12:32:13 -0400

    Add files via upload

---
+++ vmdiagdeploy04.25.ps1
@@ -0,0 +1,4 @@
+﻿$templatedeploy = "C:\Users\neshe\OneDrive\Desktop\Assignments\vmdiagndeploy1.json"
+New-AzResourceGroup -name workiisdeploy -Location EastUS 
+New-AzResourceGroupDeployment -Name workiisdeploy -ResourceGroupName workiisdeploy -TemplateFile $templatedeploy
+
