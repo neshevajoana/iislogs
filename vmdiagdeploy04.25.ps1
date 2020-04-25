ab5383c531ccd57e29eeae095fe0aa6bc353550c
neshevajoana <53709092+neshevajoana@users.noreply.github.com>
2020-04-25 16:38:07 -0400

    Update vmdiagdeploy04.25.ps1

--- vmdiagdeploy04.25.ps1
+++ vmdiagdeploy04.25.ps1
@@ -1,3 +1,3 @@
 $templatedeploy = "https://raw.githubusercontent.com/neshevajoana/iislogs/master/vmdiagndeploy04.25.json"
-New-AzResourceGroup -name workiisdeploy -Location EastUS 
+New-AzResourceGroup -name workiisdeploy -Location EastUS -Force
 New-AzResourceGroupDeployment -Name workiisdeploy -ResourceGroupName workiisdeploy -TemplateUri $templatedeploy -Force
